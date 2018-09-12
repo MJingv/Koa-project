b3711d3f39409cdbb0a965a9825cd4057a345d41
MJingv <625619954@qq.com>
2018-09-12 16:34:28 +0800

    初始化

--- server/index.js
+++ server/index.js
@@ -1,26 +1,10 @@
 const Koa = require('koa')
-const session = require('koa-session')
 const app = new Koa()
 
 
-// app.use(async (ctx, next) => {
-//     console.log(ctx.host)
-//     ctx.body = 'Hello jehol';
-// });
-app.keys = ['jehol']
-app.use(session(app))
+app.use(async (ctx, next) => {
+    ctx.body = 'Hello jehol';
+});
 
-app.use(ctx => {
-    if (ctx.path === '/') {
-        if (ctx.path === '/favicon.ico') return
-        let n = ctx.session.views || 0
-        ctx.session.views = ++n
-        ctx.body = n + 'views'
-
-    } else if (ctx.path === '/hello') {
-        ctx.body = 'hello world'
-
-    }
-})
 
 app.listen(2333)
