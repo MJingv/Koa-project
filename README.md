af30ef83be97cb27d54c2d7144349dfdc0bf6d66
MJingv <625619954@qq.com>
2019-02-10 10:50:29 +0800

    +style

--- README.md
+++ README.md
@@ -3,11 +3,12 @@
 ---
 [koa 学习笔记](https://github.com/MJingv/Koa-project/blob/master/koa%E5%AD%A6%E4%B9%A0%E7%AC%94%E8%AE%B0.md)
 
-增加模版引擎
-===
+#### 增加模版引擎
 
-> ejs vs pug
-> >可传递变量，语法不同，pug更简洁
+> 1. 简单的html展示
+> 1. koa-view:请求html页面时，后台会用模板引擎渲染数据到模板上，然后返回给后台
+> 1. ejs vs pug
+
 
 
 
--- server/index.js
+++ server/index.js
@@ -1,18 +1,22 @@
 const Koa = require('koa');
 const app = new Koa();
-const {htmlTpl, ejsTpl, pugTpl} = require('./tpl/index');
-const ejs = require('ejs');
 const pug = require('pug');
+const views = require('koa-views');
+const {resolve} = require('path');
+
+
 // const {connect} = require('./database/init')
 
 // ;(async () => {
 // 	await connect();
 // })();
+app.use(views(resolve(__dirname, './views'), {
+	extension: 'pug'
+}));
 
 
 app.use(async (ctx, next) => {
-	ctx.type = 'text/html; charset=utf-8';
-	ctx.body = pug.render(pugTpl, {
+	await ctx.render('index', {
 		name: 'jehol',
 		pageTitle: '电影预告片'
 	});
