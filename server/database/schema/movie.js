051bfdcc2c533e64edadbad7b6e854dd753df1cf
MJingv <625619954@qq.com>
2019-02-11 09:42:12 +0800

    +movie-schema

--- server/database/schema/movie.js
+++ server/database/schema/movie.js
@@ -1,18 +1,38 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
+const Mixed = Schema.Types.Mixed;
 
 const movieSchema = new Schema({
-    doubanId: String,
-    rate: Number,
-    title: String,
-    summary: String,
-    video: String,
-    poster: String,
-    cover: String,
+	doubanId: String,
+	rate: Number,
+	title: String,
+	summary: String,
+	video: String,
+	poster: String,
+	cover: String,
 
-    rawTitle: String,
-    movieTypes: [String],
-    pubdate: Mixed,
+	//七牛资源key
+	videoKey: String,
+	posterKey: String,
+	coverKey: String,
 
+	rawTitle: String,
+	movieTypes: [String],
+	pubdate: Mixed,
+	year: Number,
 
-})
+	tags: Array,
+
+	meta: {
+		createAt: {
+			type: Date,
+			default: Date.now(),
+		},
+		updateAt: {
+			type: Date,
+			default: Date.now()
+		}
+	}
+});
+
+mongoose.model('Movie', movieSchema);
